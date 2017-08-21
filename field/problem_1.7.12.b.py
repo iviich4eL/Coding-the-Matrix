0dfd4cb165f7bdf4ef65f0b1ddbe5e6379e6bc95
Michael <Mich4el@protonmail.com>
2017-08-21 15:44:16 +0300

    Rename variables

--- field/problem_1.7.12.b.py
+++ field/problem_1.7.12.b.py
@@ -1,4 +1,4 @@
-def transform(a,b,L) : return [ a * z + b for z in L ]
+def transform(a, b, L) : return [ a * z + b for z in L ]
 
 S = [2+1j]#,2+2j, 3+2j, 1.75+1j, 2+1j, 2.25+1j, 2.5+1j, 2.75+1j, 3+1j, 3.25+1j]
 print(S)
