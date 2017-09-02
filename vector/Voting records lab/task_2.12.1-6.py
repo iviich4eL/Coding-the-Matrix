07428807f94bd59b96297dffc9335470af7314f5
Unknown <misha98tiger@yandex.ru>
2017-09-02 19:08:14 +0300

    Update /Voting_records_lab

--- vector/Voting records lab/task_2.12.1-6.py
+++ vector/Voting records lab/task_2.12.1-6.py
@@ -20,26 +20,22 @@ def most_similar(sen, voting_dict):
     senRes = 'None'
     mindset = -100000000 #policy_compare(sen,sen, voting_dict)
     for name in voting_dict:
-        if name != sen: 
-            policyComp = policy_compare(name,sen, voting_dict) 
-            if policyComp > mindset:
-                mindset = policyComp
-                senRes = name
+        if name != sen and policy_compare(name, sen, voting_dict) >= mindset:
+            mindset = policy_compare(name, sen, voting_dict)
+            senRes = name
     return senRes
 
 def least_similar(sen, voting_dict): 
     senRes = 'None'
     mindset = 100000000 #policy_compare(sen,sen, voting_dict)
     for name in voting_dict:
-        if name != sen: 
-            policyComp = policy_compare(name,sen, voting_dict) 
-            if policyComp < mindset:
-                mindset = policyComp
-                senRes = name
+        if name != sen and policy_compare(name,sen, voting_dict) <= mindset:
+            mindset = policy_compare(name,sen, voting_dict)
+            senRes = name
     return senRes
 
 def find_average_similarity(sen, sen_set, voting_dict):
-    return  int(sum([policy_compare(sen,x, voting_dict) for x in sen_set]) / len(sen_set))
+    return  int(sum([policy_compare(sen, x, voting_dict) for x in sen_set]) / len(sen_set))
 
 def create_democrate_set():
     f = open('voting_record_dump109.txt')
@@ -68,17 +64,75 @@ def agree_with_democrats(voting_dict):
                 resName = name
     return resName
 
-def addN(v, w): [v[n] + w[n] for n in range(len(v))]
+def addN(v, w): return [v[n] + w[n] for n in range(len(v))]
 def zero_vec(size): return [0 for x in range(size)]
 def find_average_record(sen_set, voting_dict):
-    res = zero_vec()
-    return int( sum ([addN(voting_dict[name], res) for name in sen_set]) / len(sen_set))
+    res = zero_vec(len(myVotingDict['Warner']))
+    for name in sen_set:
+        res = addN(voting_dict[name], res)
+    return [ x / len(sen_set) for x in res ]
 
-myVotingDict = create_voting_dict(mylist)
+def most_similar_to_average_Democrats(average_Democrat_record, voting_dict):
+    res = -float('infinity')
+    resName = 'None'
+    for name in voting_dict:
+        if dot_product(voting_dict[name], average_Democrat_record) >= res:
+            res = dot_product(voting_dict[name], average_Democrat_record)
+            resName = name
+    return resName 
+
+
+def bitter_rivals(voting_dict):
+    res = float('infinity')
+    resName = 'None'
+    flag = 0
+    for sen_a in voting_dict:
+        for sen_b in voting_dict:
+            if sen_a != sen_b and policy_compare(sen_a, sen_b, voting_dict) < res:
+                res = policy_compare(sen_a, sen_b, voting_dict) 
+                x = sen_a
+                y = sen_b
+    return (x, y)
+
+def create_rivals_dict(voting_dict):
+    return { name : int(0) for name in voting_dict}
+def list2dict(D): return [ D[x] for x in D ]
+def exchange_in_dict(D) : return { D[x] : x for x in D}
+def find_max_in_dict(sen_dict):
+    value_list = list2dict(sen_dict)
+    value_list.sort()
+    sen_dict = exchange_in_dict(sen_dict)
+    return sen_dict[value_list[len(value_list) - 1]]
+
+def most_political_opponents(voting_dict):
+    treshold = 0
+    sen_dict = create_rivals_dict(voting_dict)
+    for sen_a in voting_dict:
+        for sen_b in voting_dict:
+            if sen_a != sen_b and policy_compare(sen_a, sen_b, voting_dict) < treshold:
+                sen_dict[sen_a] = sen_dict[sen_a] + 1
+    # print(sen_dict)
+    return find_max_in_dict(sen_dict)
+
+
+myVotingDict = create_voting_dict(mylist) 
+average_Democrat_record = find_average_record(create_democrate_set(),myVotingDict)
+
+print('Chafee most agrees with',most_similar('Chafee', myVotingDict))
+print(least_similar('Santorum',myVotingDict),'most disagrees with Santorum')
 print('Most agree with democrates:', agree_with_democrats(myVotingDict))
-
-# print('Chafee most agrees with', most_similar('Chafee', myVotingDict))
-# print(least_similar('Santorum',myVotingDict),'most disagrees with Santorum')
+print('Most agree with democrates v2:', most_similar_to_average_Democrats(average_Democrat_record, myVotingDict))
+print( most_political_opponents(myVotingDict),'has most political opponents')
+# # print('Find average record:', average_Democrat_record)
+# # myVotingDict = {'Klein': [-1,0,1], 'Fox-Epstein': [-1,-1,-1], 'Ravella': [0,0,1]}
+# print('Bitter rivals: ', bitter_rivals(myVotingDict))
+# test = {'apple' : 1, 'banana' : 0, 'orange' : 2}
+# print(list2dict(test))
+# print(find_min_in_dict(test))
+# print(exchange_in_dict(test))
+# print(test.values())
+# M=-float('infinity')
+# print(M)
 # print('Schumer', 'Collins',policy_compare('Schumer', 'Collins', myVotingDict))
 # print(create_democrate_set())
 # print(create_voting_dict(mylist))
